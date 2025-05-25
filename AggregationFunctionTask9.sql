01c68917bed497f1d63d46cf54c57c3b3e9b7b29
Amani Khalid <amanikhalidd@gmail.com>
2025-05-25 09:17:29 +0400

    Added Library Database Aggregation Functions

--- AggregationFunctionTask9.sql
+++ AggregationFunctionTask9.sql
@@ -121,7 +121,31 @@ SELECT Customer_ID, SUM(Amount) AS total_loan
 FROM Loan
 GROUP BY Customer_ID
 HAVING SUM(Amount) > 200000;
+---------------------
+--Library Database 
+use LibraryDB;
+--26. Count total books in Books table.
+SELECT COUNT(*) AS total_books FROM Book;
 
+--27. Average book price from Books table. 
+SELECT AVG(Price) AS average_price FROM Book;
+
+--28. Count books per library grouped by Library_ID. 
+SELECT L_ID, COUNT(*) AS book_count
+FROM Book
+GROUP BY L_ID;
+
+
+--29. Count books borrowed per member from Borrows table grouped by Member_ID. 
+SELECT B_ID, COUNT(*) AS books_borrowed
+FROM Ownership
+GROUP BY B_ID;
+
+--30. List members who borrowed more than 3 books grouped by Member_ID.
+SELECT B_ID, COUNT(*) AS books_borrowed
+FROM Ownership
+GROUP BY B_ID
+HAVING COUNT(*) > 3;
 
 
 
